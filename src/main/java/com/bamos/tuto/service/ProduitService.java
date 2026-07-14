497160ea1ca74ac5c00ee59067bdbcc438a5ed25
unknown <AbdallahBAMOGO>
2026-07-14 11:04:24 +0000

    Création de la couche Service

---
+++ src/main/java/com/bamos/tuto/service/ProduitService.java
@@ -0,0 +1,14 @@
+package com.bamos.tuto.service;
+
+import com.bamos.tuto.model.Produit;
+
+import java.util.List;
+
+public interface ProduitService {
+    Produit saveProduit(Produit p);
+    Produit updateProduit(Produit p);
+    void deleteProduit(Produit p);
+    void deleteProduitById(Long id);
+    Produit getProduit(Long id);
+    List<Produit>  getAllProduits();
+}
