2af9673d4a82999babb27a11335fe6b844400f56
AbdallahBAMOGO <abdallahbamogo17@gmail.com>
2026-07-14 11:47:41 +0000

    Utilisation de Lombok

--- src/main/java/com/bamos/tuto/model/Categorie.java
+++ src/main/java/com/bamos/tuto/model/Categorie.java
@@ -1,9 +1,16 @@
 package com.bamos.tuto.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Categorie {
     @Id
@@ -11,38 +18,8 @@ public class Categorie {
     private Long idCat;
     private String nomCat;
     private String descriptionCat;
+
+    @JsonIgnore
     @OneToMany(mappedBy = "categorie")
     private List<Produit> produits;
-    public Categorie() {}
-    public Categorie(String nomCat, String descriptionCat, List<Produit> produits)
-    {
-        super();
-        this.nomCat = nomCat;
-        this.descriptionCat = descriptionCat;
-        this.produits = produits;
-    }
-    public Long getIdCat() {
-        return idCat;
-    }
-    public void setIdCat(Long idCat) {
-        this.idCat = idCat;
-    }
-    public String getNomCat() {
-        return nomCat;
-    }
-    public void setNomCat(String nomCat) {
-        this.nomCat = nomCat;
-    }
-    public String getDescriptionCat() {
-        return descriptionCat;
-    }
-    public void setDescriptionCat(String descriptionCat) {
-        this.descriptionCat = descriptionCat;
-    }
-    public List<Produit> getProduits() {
-        return produits;
-    }
-    public void setProduits(List<Produit> produits) {
-        this.produits = produits;
-    }
 }
